28d770038abbdd0d7cac5f1adc5e34348cd93ee0
yonko <nexiathon@gmail.com>
2024-06-07 15:54:09 +0200

    leaderboard avancement

--- src/leaderboard.ts
+++ src/leaderboard.ts
@@ -1,3 +1,17 @@
-export class leaderboards{
-    
+export class leaderboards {
+  private scores: { [key: string]: number };
+
+  constructor() {
+    this.scores = { player: 0, computer: 0, tie: 0 };
+  }
+
+  updateScores(result: string): void {
+    if (result in this.scores) {
+      this.scores[result]++;
+    }
+  }
+
+  getScores(): { [key: string]: number } {
+    return this.scores;
+  }
 }
